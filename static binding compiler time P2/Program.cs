4b97d3ad3f5d1fa0b98c142bc4fbf874956ad265
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-22 01:55:41 +0530

    Add project files.

---
+++ static binding compiler time P2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace static_binding_compiler_time_P2
+{
+    class MyClass
+    {
+        public int x = 100;
+    }
+    class Program:MyClass
+    {
+        public int x = 200;
+
+        static void Main(string[] args)
+        {
+            
+            MyClass obj = new Program();
+            Console.WriteLine(obj.x+" "+obj.x);
+        }
+    }
+}
